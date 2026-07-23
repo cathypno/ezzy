fbe55aa1225e327c7db8674cd62ffde6f844e1bd
Denis Korneev <veenrock@gmail.com>
2026-07-23 18:53:17 +0300

    fix colors

--- app/components/ezcord/EzcordAuthScreen.vue
+++ app/components/ezcord/EzcordAuthScreen.vue
@@ -90,11 +90,18 @@ const emit = defineEmits<{
       </form>
 
       <button
-        class="mt-3.5 inline-flex min-h-[50px] w-full items-center justify-center rounded-[14px] border border-ez-line bg-ez-card px-[18px] text-[15px] font-black text-ez-ink transition hover:-translate-y-px disabled:cursor-default disabled:opacity-[.58] disabled:hover:translate-y-0"
+        class="mt-3.5 inline-flex min-h-[50px] w-full items-center justify-center gap-2.5 rounded-[14px] border border-transparent bg-[#229ed9] px-[18px] text-[15px] font-black text-white shadow-[0_12px_26px_-14px_rgba(34,158,217,0.9)] transition hover:-translate-y-px hover:bg-[#1f93c9] disabled:cursor-default disabled:opacity-[.58] disabled:hover:translate-y-0"
         :disabled="props.telegramLoginLoading"
         type="button"
         @click="emit('telegram-login')"
       >
+        <svg class="h-5 w-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
+          <path d="M0 0h24v24H0z" fill="none" />
+          <g fill="none" fill-rule="evenodd">
+            <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.004-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+            <path fill="currentColor" d="M19.777 4.43a1.5 1.5 0 0 1 2.062 1.626l-2.268 13.757c-.22 1.327-1.676 2.088-2.893 1.427c-1.018-.553-2.53-1.405-3.89-2.294c-.68-.445-2.763-1.87-2.507-2.884c.22-.867 3.72-4.125 5.72-6.062c.785-.761.427-1.2-.5-.5c-2.302 1.738-5.998 4.381-7.22 5.125c-1.078.656-1.64.768-2.312.656c-1.226-.204-2.363-.52-3.291-.905c-1.254-.52-1.193-2.244-.001-2.746z" />
+          </g>
+        </svg>
         {{ props.telegramLoginLoading ? "Ждем подтверждение..." : "Войти через Telegram" }}
       </button>
       <a
